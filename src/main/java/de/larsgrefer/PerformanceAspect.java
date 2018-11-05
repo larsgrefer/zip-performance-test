89f87576d37e49a7da09582893fb8f61e033eef5
Lars Grefer <github@larsgrefer.de>
2018-11-05 21:33:28 +0100

    Update Aspect

--- src/main/java/de/larsgrefer/PerformanceAspect.java
+++ src/main/java/de/larsgrefer/PerformanceAspect.java
@@ -3,6 +3,7 @@ package de.larsgrefer;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -18,12 +19,21 @@ public class PerformanceAspect {
         long start = System.nanoTime();
         try {
             return proceedingJoinPoint.proceed();
-        } catch (Throwable throwable) {
-            throw throwable;
         } finally {
             times.computeIfAbsent(proceedingJoinPoint.getSignature().toLongString(), k -> new LinkedList<>())
                     .add(System.nanoTime() - start);
         }
+    }
+
+    public static void printStats() {
+        times.entrySet().stream()
+                .sorted(Comparator.comparing(Map.Entry::getKey))
+                .forEach(stringListEntry -> {
+                    String signature = stringListEntry.getKey();
+                    double avgMs = stringListEntry.getValue().stream().mapToLong(Long::longValue).average().orElse(0d) / 1000000d;
+                    double sumMs = stringListEntry.getValue().stream().mapToLong(Long::longValue).sum() / 1000000d;
+                    System.out.printf("%s -> %f ms total (%f ms avg)\n", signature, sumMs, avgMs);
+                });
     }
 
 }
